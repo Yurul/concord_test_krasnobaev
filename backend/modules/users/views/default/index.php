6785f3b1948aa007d7a7eb3ea85619836bf1b89f
Yurul <yusyabox@gmail.com>
2019-05-19 17:37:19 +0300

    karkas of users module

---
+++ backend/modules/users/views/default/index.php
@@ -0,0 +1,13 @@
+<div class="users-default-index">
+    <h1>User managment module</h1>
+    <p>
+        - Link
+    </p>
+    <p>
+        - Link
+    </p>
+    <p>
+        - Link
+    </p>
+  
+</div>
